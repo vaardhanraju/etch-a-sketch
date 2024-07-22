f3eedbe6b7f46f7818640036316da4f3091a273d
vaardhanraju <vaardhanraju@gmail.com>
2024-07-22 14:39:36 +0530

    Grid size manipulation.
    Added event listener to the grid size button where user can select the grid size of his choice between the range of 1 to 100.
    
    Then the container automatically changes into n*n size grid.

--- script.js
+++ script.js
@@ -18,3 +18,32 @@ for (let i = 0; i < gridSize; i++) {
         container.appendChild(grid);
     }
 }
+
+
+const gridSizeBtn = document.getElementById("grid-size")
+gridSizeBtn.addEventListener('click', () => {
+    gridSize = prompt("You can select grid size from 1 to 100");
+
+    while (gridSize < 1 || gridSize > 100) {
+        gridSize = prompt("You can select grid size from 1 to 100");
+    }
+    
+    const cells = document.querySelectorAll(".grid");
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].remove(".grid")
+    }
+    cellSize = 500/gridSize;
+    
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const grid = document.createElement("div");
+            grid.classList.add("grid");
+            grid.style.backgroundColor = "White";
+            grid.style.boxSizing = "border-box";
+            grid.style.width = `${cellSize}px`;
+            grid.style.height = `${cellSize}px`;
+            grid.style.border = "1px solid gray"
+            container.appendChild(grid);
+        }
+    }
+});
